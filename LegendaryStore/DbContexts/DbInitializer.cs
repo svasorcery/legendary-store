8c7166e15d3d933ada459b4b4d5060e8f981fe5e
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-01-27 18:35:23 +0300

    [+] DbContext : Seed database on app start

---
+++ LegendaryStore/DbContexts/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using LegendaryStore.Entities;
+
+namespace LegendaryStore.DbContexts
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any categories
+            if (context.Categories.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            var categories = new Category[]
+            {
+                new Category { Name = "Nonfiction", ParentId = null },
+                new Category { Name = "Computer Science", ParentId = 1 },
+                new Category { Name = "Software Architecture", ParentId = 2 },
+                new Category { Name = "Databases", ParentId = 2 },
+                new Category { Name = "SQL", ParentId = 4 },
+                new Category { Name = "No-SQL", ParentId = 4 },
+                new Category { Name = "Languages", ParentId = 2 },
+                new Category { Name = "C, C++, C#", ParentId = 7 },
+                new Category { Name = "Java", ParentId = 7 },
+                new Category { Name = "JavaScript", ParentId = 7 },
+                new Category { Name = "Python", ParentId = 7 },
+            };
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            var products = new Product[]
+            {
+
+            };
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+        }
+    }
+}
--- LegendaryStore/Program.cs
+++ LegendaryStore/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LegendaryStore
 {
@@ -14,7 +10,24 @@ namespace LegendaryStore
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DbContexts.StoreDbContext>();
+                    DbContexts.DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
