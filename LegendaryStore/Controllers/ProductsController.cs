37b3abce0e48bce3ad0c31c9cbd936e60be33c54
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-01-29 17:08:58 +0300

    [+] ProductsController : added Service and Controller with CRUD-operation methods for Products

---
+++ LegendaryStore/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegendaryStore.Controllers
+{
+    using LegendaryStore.Entities;
+    using LegendaryStore.Services;
+    
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreDbService _db;
+
+        public ProductsController(StoreDbService dbService)
+        {
+            _db = dbService;
+        }
+
+
+        [HttpGet("by-category/{categoryId:int}")]
+        public async Task<IActionResult> ByCategory([FromRoute]int categoryId)
+        {
+            var items = await _db.GetProductsAsync(categoryId);
+
+            return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            var model = await _db.GetProductAsync(id);
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Product model)
+        {
+            try
+            {
+                var id = await _db.CreateProductAsync(model);
+                model.Id = id;
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Edit([FromRoute]int id, [FromBody]Product model)
+        {
+            try
+            {
+                await _db.UpdateProductAsync(id, model);
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute]int id)
+        {
+            try
+            {
+                await _db.RemoveProductAsync(id);
+
+                return Ok(new { deleted = true });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}
---
+++ LegendaryStore/Services/StoreDbService.Products.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LegendaryStore.Entities;
+
+namespace LegendaryStore.Services
+{
+    public partial class StoreDbService
+    {
+        public Task<Product[]> GetProductsAsync(int categoryId)
+        {
+            return _storeDb.Products
+                .Where(x => x.CategoryId == categoryId)
+                .ToArrayAsync();
+        }
+
+        public Task<Product> GetProductAsync(int id)
+        {
+            return _storeDb.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<int> CreateProductAsync(Product model)
+        {
+            _storeDb.Products.Add(model);
+
+            return _storeDb.SaveChangesAsync();
+        }
+
+        public async Task<Product> UpdateProductAsync(int id, Product model)
+        {
+            var update = await GetProductAsync(id);
+
+            update.Name = model.Name;
+            update.Description = model.Description;
+            update.Price = model.Price;
+            update.Quantity = model.Quantity;
+            update.ImageUrl = model.ImageUrl;
+
+            _storeDb.Products.Update(update);
+            await _storeDb.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task RemoveProductAsync(int id)
+        {
+            var remove = await GetProductAsync(id);
+
+            _storeDb.Products.Remove(remove);
+            await _storeDb.SaveChangesAsync();
+        }
+    }
+}
