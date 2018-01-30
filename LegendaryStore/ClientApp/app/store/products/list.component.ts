66abcd5d2e13a1a8338406c36caaa0cb2bdfd942
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-01-30 20:39:56 +0300

    [*] ProductsListComponent : Added items sorting

--- LegendaryStore/ClientApp/app/store/products/list.component.ts
+++ LegendaryStore/ClientApp/app/store/products/list.component.ts
@@ -26,4 +26,22 @@ export class ProductsListComponent implements OnInit {
                 error => console.log(error)
             );
     }
+
+    /* sorting */
+
+    orderBy: string = null;
+
+    sortFn = {
+        name: function(a: Product, b: Product) {
+            return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+        },
+        price: function(a: Product, b: Product) {
+            return a.price > b.price ? 1 : a.price < b.price ? -1 : 0;
+        }
+    };
+
+    sort(kind: string) {
+        this.items.sort(this.sortFn[kind]);
+        this.orderBy = kind;
+    }
 }
