cce41c7dcd52bf9f1fb232df0327fc1dffb0d13b
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-01-30 18:07:30 +0300

    [+] SharedModule : Added AutoCompleteComponent for dynamic searching data

--- LegendaryStore/ClientApp/app/shared/shared.module.ts
+++ LegendaryStore/ClientApp/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { SpinnerComponent } from './spinner.component';
+import { AutoCompleteComponent } from './autocomplete.component';
 import { TreeMenuComponent, TreeMenuItemComponent } from './tree-menu.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ],
     declarations: [
         SpinnerComponent,
+        AutoCompleteComponent,
         TreeMenuItemComponent,
         TreeMenuComponent
     ],
@@ -21,6 +25,7 @@ import { TreeMenuComponent, TreeMenuItemComponent } from './tree-menu.component'
     exports: [
         CommonModule,
         SpinnerComponent,
+        AutoCompleteComponent,
         TreeMenuItemComponent,
         TreeMenuComponent
     ],
