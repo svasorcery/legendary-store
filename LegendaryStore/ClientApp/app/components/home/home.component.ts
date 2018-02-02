e61726b701630f3767a38cc90afe53818492c3ce
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-02 23:08:52 +0300

    [*] HomeComponent : Added Products AutoComplete and selected product card

--- LegendaryStore/ClientApp/app/components/home/home.component.ts
+++ LegendaryStore/ClientApp/app/components/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { ProductsService } from '../../store/products/products.service';
+import { Product } from '../../store/store.models';
+
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    
+    product: Product;
+
+    constructor(public products: ProductsService) {
+
+    }
 }
