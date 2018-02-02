53b5d92f05dcbce989a6b8e7b607935278be50bc
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-02 23:07:56 +0300

    [*] ProductsService : Added ProductsListSource instance

--- LegendaryStore/ClientApp/app/store/products/products.service.ts
+++ LegendaryStore/ClientApp/app/store/products/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, URLSearchParams } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -12,12 +12,18 @@ import { ProductsList, Product } from '../store.models';
 @Injectable()
 export class ProductsService {
     private _url: string;
+    private _productsSource: ProductsListSource;
+
+    public get productsSource(): ProductsListSource {
+        return this._productsSource;
+    }
 
     constructor(
         private _http: Http,
         @Inject('BASE_URL') baseUrl
     ) { 
         this._url = baseUrl + 'api/products';
+        this._productsSource = new ProductsListSource(_http, baseUrl);
     }
     
     public getProducts(categoryId: number, page: number = 1): Observable<ProductsList> {
@@ -37,7 +43,7 @@ export class ProductsListSource implements IAutoCompleteListSource {
     search(term: string): Observable<{ name: string }[]> {
         let params = new URLSearchParams();
         params.set('term', term);
-        return this.http.get(this.baseUrl + '/api/products/search', { search: params })
+        return this.http.get(this.baseUrl + 'api/products/search', { search: params })
             .map((response: Response) => response.json() as Product[]);
     }
 }
