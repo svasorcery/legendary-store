7df275ba21e65e012de018f69b98540cece81e69
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-15 22:24:55 +0300

    [-] HomeController : removed AuthorizeAttribute

--- LegendaryStore/Controllers/HomeController.cs
+++ LegendaryStore/Controllers/HomeController.cs
@@ -4,11 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 namespace LegendaryStore.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
