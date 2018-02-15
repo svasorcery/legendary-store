ecc47fe1447e092dc2e176b0056d14c3303a7334
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-15 23:25:59 +0300

    [*] NavMenuComponent : added logged-in userName and user-avatar to nav menu

--- LegendaryStore/ClientApp/app/components/navmenu/navmenu.component.ts
+++ LegendaryStore/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { UserService } from '../../user.service';
+import { UserInfo } from '../../app.models';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
     styleUrls: ['./navmenu.component.css']
 })
-export class NavMenuComponent {
-    
+export class NavMenuComponent implements OnInit {
+    userInfo: UserInfo;
+
+    constructor(private _user: UserService) {}
+
+    ngOnInit() {
+        this._user.WhoAmI()
+            .subscribe(
+                result => this.userInfo = result,
+                error => console.log(error)
+            );
+    }
 }
