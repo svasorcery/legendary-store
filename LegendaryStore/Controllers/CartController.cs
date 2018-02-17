b48cf6101fa90e0abbcbacb980e924d583a8d796
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-17 20:45:12 +0300

    [+] CartController : Created CartController with Get, Add, Remove methods

---
+++ LegendaryStore/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LegendaryStore.Controllers
+{
+    using LegendaryStore.Services;
+
+    [Authorize]
+    [Route("api/[controller]")]
+    public class CartController : Controller
+    {
+        private readonly StoreDbService _db;
+
+        public CartController(StoreDbService dbService)
+        {
+            _db = dbService;
+        }
+
+
+        public async Task<IActionResult> Get()
+        {
+            var cart = await _db.GetCartItemsAsync();
+
+            return Ok(cart);
+        }
+
+        [HttpGet("{productId:int}")]
+        public async Task<IActionResult> Add(int productId)
+        {
+            var product = await _db.GetProductAsync(productId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _db.AddToCartAsync(product);
+
+            return Ok(item);
+        }
+
+        [HttpGet("{productId:int}")]
+        public async Task<IActionResult> Remove(int productId)
+        {
+            var item = await _db.GetCarItemAsync(productId);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            await _db.RemoveFromCartAsync(item);
+
+            return Ok(item);
+        }
+    }
+}
