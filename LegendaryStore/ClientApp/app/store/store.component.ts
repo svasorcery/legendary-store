5e1b72b3386ff7f63a4fb1dfd693d11a7d9b0f20
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-18 16:12:25 +0300

    [*] StoreComponent : Added modal for showing cart

--- LegendaryStore/ClientApp/app/store/store.component.ts
+++ LegendaryStore/ClientApp/app/store/store.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'store',
     template: `
-        <h2>Legendary Store's catalog</h2>
+        <h2>
+            Legendary Store's catalog
+            <button (click)="showCart()" class="btn btn-default pull-right">
+                <i class="fa fa-shopping-cart"></i> Cart
+            </button>
+        </h2>
+
+        <modal [visible]="modal"
+            [closable]="true" 
+            [header]="'Cart'"
+            (visibleChange)="hideCart()">
+            <cart></cart>
+        </modal>
+
         <router-outlet></router-outlet>
     `
 })
 
 export class StoreComponent implements OnInit {
+    modal: boolean;
+
+    public showCart () {
+        this.modal = true;
+    }
+
+    public hideCart() {
+        this.modal = false;
+    }
+
     constructor() { }
 
     ngOnInit() { }
