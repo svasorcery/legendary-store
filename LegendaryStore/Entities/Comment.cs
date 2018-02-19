793d95c1aacc3432fe668d2643d72f7dd308ea28
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-19 22:42:28 +0300

    [+] StoreDbService : Added CommentsService

---
+++ LegendaryStore/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LegendaryStore.Entities
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+        public int ProductId { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime PostedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ LegendaryStore/Services/StoreDbService.Comments.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using LegendaryStore.Entities;
+
+namespace LegendaryStore.Services
+{
+    public partial class StoreDbService
+    {
+        public Task<Comment[]> GetCommentsAsync(int productId)
+        {
+            return _storeDb.Comments
+                .Where(x => x.ProductId == productId && x.IsDeleted == false)
+                .ToArrayAsync();
+        }
+
+        public Task<Comment[]> GetCommentsAsync(int productId, string userName)
+        {
+            return _storeDb.Comments
+                .Where(x => x.ProductId == productId && x.Author == userName && x.IsDeleted == false)
+                .ToArrayAsync();
+        }
+
+        public Task<Comment> GetCommentAsync(long id)
+        {
+            return _storeDb.Comments
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+        }
+
+        public Task CreateCommentAsync(Comment model)
+        {
+            model.Author = _userService.GetUserName();
+            model.PostedAt = DateTime.UtcNow;
+            model.IsDeleted = false;
+
+            _storeDb.Add(model);
+            return _storeDb.SaveChangesAsync();
+        }
+
+        public async Task UpdateCommentContentAsync(long id, string content)
+        {
+            var update = await GetCommentAsync(id);
+
+            update.Content = content;
+
+            _storeDb.Update(update);
+            await _storeDb.SaveChangesAsync();
+        }
+
+        public async Task DeleteCommentAsync(long id)
+        {
+            var comment = await GetCommentAsync(id);
+
+            comment.IsDeleted = true;
+
+            _storeDb.Update(comment);
+            await _storeDb.SaveChangesAsync();
+        }
+    }
+}
