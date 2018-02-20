f67cff069f9735141bf93780395568cc3576fc46
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-02-20 17:04:03 +0300

    [*] ProductDetailsComponent : added comments fetching and display

--- LegendaryStore/ClientApp/app/store/products/details.component.ts
+++ LegendaryStore/ClientApp/app/store/products/details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
-import { Product } from '../store.models';
+import { Product, Comment } from '../store.models';
 import { ProductsService } from './products.service';
+import { CommentsService } from '../comments/comments.service';
 import { CartService } from '../cart/cart.service';
 
 @Component({
@@ -12,9 +13,11 @@ import { CartService } from '../cart/cart.service';
 
 export class ProductDetailsComponent implements OnInit {
     @Input() value: Product;
+    comments: Comment[];
 
     constructor(
         private _products: ProductsService,
+        private _comments: CommentsService,
         private _cart: CartService,
         private _route: ActivatedRoute
     ) { }
@@ -25,9 +28,14 @@ export class ProductDetailsComponent implements OnInit {
                 .params
                 .switchMap((params: Params) => this._products.getProduct(+params['id']))
                 .subscribe(
-                    result => this.value = result,
+                    result => {
+                        this.value = result;
+                        this.getComments();
+                    },
                     error => console.log(error)
                 );
+        } else {
+            this.getComments();
         }
     }
 
@@ -36,6 +44,14 @@ export class ProductDetailsComponent implements OnInit {
         this._cart.addItem(this.value.id)
             .subscribe(
                 result => console.log(`${result.product.name} added`),
+                error => console.log(error)
+            );
+    }
+
+    public getComments() {
+        this._comments.getCommentsForProduct(this.value.id)
+            .subscribe(
+                result => this.comments = result,
                 error => console.log(error)
             );
     }
