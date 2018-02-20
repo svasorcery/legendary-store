65a8e00c05c7ddefee3e643efd76d5cdcc7ea4c2
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-02-20 16:59:35 +0300

    [+] CommentsController : Created with Get method

---
+++ LegendaryStore/Controllers/CommentsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LegendaryStore.Controllers
+{
+    using LegendaryStore.Services;
+
+    [Authorize]
+    [Route("api/[controller]")]
+    public class CommentsController : Controller
+    {
+        private readonly StoreDbService _db;
+
+        public CommentsController(StoreDbService dbService)
+        {
+            _db = dbService;
+        }
+
+
+        [HttpGet("{productId:int}")]
+        public async Task<IActionResult> Get(int productId)
+        {
+            var comments = await _db.GetCommentsAsync(productId);
+
+            return Ok(comments);
+        }
+    }
+}
