bcc2ea65cc24301323f7d665cda25f9bb9b6445c
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-21 22:58:15 +0300

    [*] Favorite model : fixed Id type, added Product prop

--- LegendaryStore/Entities/Favorite.cs
+++ LegendaryStore/Entities/Favorite.cs
@@ -4,8 +4,9 @@ namespace LegendaryStore.Entities
 {
     public class Favorite
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public int ProductId { get; set; }
+        public Product Product { get; set; }
         public string UserName { get; set; }
         public DateTime AddedAt { get; set; }
     }
