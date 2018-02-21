3543d77583a9e54dd7da4d6a338c3df6316f690a
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-21 21:55:28 +0300

    [+] StoreDbService : Added FavoritesService

---
+++ LegendaryStore/Services/StoreDbService.Favorites.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LegendaryStore.Entities;
+
+namespace LegendaryStore.Services
+{
+    public partial class StoreDbService
+    {
+        public Task<Favorite[]> GetFavoritesAsync()
+        {
+            return _storeDb.Favorites
+                .Where(i => i.UserName == _userService.GetUserName())
+                .ToArrayAsync();
+        }
+
+        public Task<Favorite> GetFavoriteAsync(int productId)
+        {
+            var userName = _userService.GetUserName();
+
+            return _storeDb.Favorites
+                .SingleOrDefaultAsync(i => i.ProductId == productId
+                                        && i.UserName == userName);
+        }
+
+        public async Task<bool> IsFavoriteAsync(int productId)
+        {
+            return (await GetFavoriteAsync(productId)) != null;
+        }
+
+        public async Task<Favorite> AddToFavoritesAsync(Product product)
+        {
+            var userName = _userService.GetUserName();
+
+            var favorite = new Favorite
+            {
+                ProductId = product.Id,
+                UserName = userName,
+                AddedAt = DateTime.Now.ToUniversalTime()
+            };
+
+            var entity = _storeDb.Favorites.Add(favorite).Entity;
+
+            await _storeDb.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public Task RemoveFromFavoritesAsync(Favorite item)
+        {
+            _storeDb.Favorites.Remove(item);
+
+            return _storeDb.SaveChangesAsync();
+        }
+    }
+}
