840f830190e8221153e03035e790de6cd93becaf
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-02-22 22:34:30 +0300

    [+] StoreDbContext : Added Rating DbSet

--- LegendaryStore/DbContexts/StoreDbContext.cs
+++ LegendaryStore/DbContexts/StoreDbContext.cs
@@ -15,6 +15,7 @@ namespace LegendaryStore.DbContexts
         public DbSet<CartItem> CartItems { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Rating> Rating { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ LegendaryStore/Entities/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LegendaryStore.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string UserName { get; set; }
+        public RatingRate Rate { get; set; }
+        public DateTime RatedAt { get; set; }
+    }
+
+    public enum RatingRate
+    {
+        Poor = 1,
+        Fair = 2,
+        Average = 3,
+        Good = 4,
+        Excellent = 5
+    }
+}
